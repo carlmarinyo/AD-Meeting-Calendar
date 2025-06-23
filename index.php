c87b0d4f2e5a1df9d36324b8aab04f1365cc07ae
carlmarinyo <marinojerome2@gmail.com>
2025-06-23 19:35:28 +0800

    refactor(landing-page): require bootstrap.php and used custom path of handlers

--- index.php
+++ index.php
@@ -7,10 +7,9 @@
 </head>
 <body>
     <?php 
-    define('HANDLERS_PATH', __DIR__ . '/handlers');
-       include_once HANDLERS_PATH . "/mongodbChecker.handler.php";
-        include_once HANDLERS_PATH . "/postgreChecker.handler.php"
-    
+require_once 'bootstrap.php';
+require_once HANDLERS_PATH . 'mongodbChecker.handler.php';
+require_once HANDLERS_PATH . 'postgreChecker.handler.php';
  
     
     
