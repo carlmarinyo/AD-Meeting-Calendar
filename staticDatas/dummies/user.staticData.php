35287fb6e76e8173c214eef81be14f35491753ed
carlmarinyo <marinojerome2@gmail.com>
2025-07-05 23:34:36 +0800

    feat(user.staticData): add more user static data

--- staticDatas/dummies/user.staticData.php
+++ staticDatas/dummies/user.staticData.php
@@ -1,9 +1,20 @@
 <?php
 // the table of users are compose of following columns: id, username, first_name, last_name, password, role
 return [
-    ['username' => 'john.smith', 
-    'first_name' => 'John', 
-    'last_name' => 'Smith', 
-    'password' => 'p@ssW0rd1234', 
-    'role' => 'designer'],
+    ['username' => 'mverstappen', 
+    'first_name' => 'Max', 
+    'last_name' => 'Verstappen', 
+    'password' => 'champ', 
+    'role' => 'Leader'],
+    ['username' => 'lhamilton', 
+    'first_name' => 'Lewis', 
+    'last_name' => 'Hamilton', 
+    'password' => 'seven', 
+    'role' => 'Member'],
+    ['username' => 'cmarino', 
+    'first_name' => 'Carl', 
+    'last_name' => 'Marino', 
+    'password' => '123', 
+    'role' => 'Member']
+
 ];
