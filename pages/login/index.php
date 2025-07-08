04adf23b61b97a1bdaba1ca82b81605e25214599
carlmarinyo <marinojerome2@gmail.com>
2025-07-08 11:05:35 +0800

    feat(login): create page for logged in users

---
+++ pages/login/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>SUCCESS!!</h1>
+    <form action="/handlers/auth.handler.php?action=logout" method="POST">
+        <button type="submit">Logout</button>
+    </form>
+</body>
+</html>
