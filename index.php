f0d8e81ff09b6d30c4656770cca02f9815661d82
carlmarinyo <marinojerome2@gmail.com>
2025-07-08 11:03:21 +0800

    feat(landing-page): add form for login

--- index.php
+++ index.php
@@ -10,16 +10,24 @@
 require_once 'bootstrap.php';
 require_once HANDLERS_PATH . 'mongodbChecker.handler.php';
 require_once HANDLERS_PATH . 'postgreChecker.handler.php';
- 
-    
-    
-    
-    
-    
-    
-    
-    
-    
+ $error = trim((string) ($_GET['error'] ?? ''));
+$error = str_replace("%", " ", $error);
     ?>
+
+        <form action="/handlers/auth.handler.php" method="POST">
+        <label for="username" class="label">Username</label>
+        <input id="username" name="username" type="text" required class="input">
+
+        <label for="password" class="label">Password</label>
+        <input id="password" name="password" type="password" required class="input">
+
+        <input type="hidden" name="action" value="login">
+        <button type="submit" class="button">Log In</button>
+
+        <?php if (!empty($error)): ?>
+        <div class="error"><?= htmlspecialchars($error) ?></div>
+        <?php endif; ?>
+    </form>
+    
 </body>
 </html>
