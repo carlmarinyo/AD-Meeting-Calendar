b25f6e91215d4ec3c21f43b0c56d0260c3912a5b
carlmarinyo <marinojerome2@gmail.com>
2025-07-08 21:14:37 +0800

    feat(bootstrap.php): add layouts path

--- bootstrap.php
+++ bootstrap.php
@@ -7,4 +7,5 @@ define ('DUMMIES_PATH', BASE_PATH . '/staticDatas/dummies/');
 define ('DATABASE_PATH', BASE_PATH . '/database');
 define ('TEMPLATES_PATH', BASE_PATH . '/components/templates');
 define ('COMPONENTS_PATH', BASE_PATH . '/components/componentGroup');
+define ('LAYOUTS_PATH', BASE_PATH . '/layouts/');
 chdir(BASE_PATH);
