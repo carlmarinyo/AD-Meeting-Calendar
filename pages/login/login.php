bc5874ccf49ac6ed33020c6beca5f020d7b53738
carlmarinyo <marinojerome2@gmail.com>
2025-07-08 20:26:53 +0800

    feat(login-page): add card component for links

---
+++ pages/login/login.php
@@ -0,0 +1,37 @@
+<?php
+require_once __DIR__ . '/../../bootstrap.php';
+require TEMPLATES_PATH . '/nav.component.php';
+require COMPONENTS_PATH . '/card.component.php';
+require_once UTILS_PATH . '/auth.util.php';
+
+Auth::init();
+if (!Auth::check()) {
+    header('Location: /errors/login_failed.php');
+    exit;
+}
+if (session_status() === PHP_SESSION_NONE) session_start();
+navHeader();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Meeting Calendar!!</h1>
+        <div class="card-container">
+
+
+    <?php
+    renderCard(
+    "My Profile",
+    "Manage your personal details and account preferences.",
+    "/assets/profile.jpg",
+    "/pages/profile.php"
+);  
+    ?>
+       </div>
+</body>
+</html>
