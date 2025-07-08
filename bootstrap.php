fee59f240a58ac0349aa26ca4015b0e7677533c9
carlmarinyo <marinojerome2@gmail.com>
2025-07-08 20:43:25 +0800

    feat(bootstrap.php): add components path

--- bootstrap.php
+++ bootstrap.php
@@ -6,4 +6,5 @@ define ('HANDLERS_PATH', BASE_PATH . '/handlers/');
 define ('DUMMIES_PATH', BASE_PATH . '/staticDatas/dummies/');
 define ('DATABASE_PATH', BASE_PATH . '/database');
 define ('TEMPLATES_PATH', BASE_PATH . '/components/templates');
+define ('COMPONENTS_PATH', BASE_PATH . '/components/componentGroup');
 chdir(BASE_PATH);
