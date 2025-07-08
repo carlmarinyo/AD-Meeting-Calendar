0b3d658d881896f5cb8d6f364258a540a8c3d9f1
carlmarinyo <marinojerome2@gmail.com>
2025-07-08 18:31:35 +0800

    feat(nav.component): add  auth-handler logic to the logout button

--- components/templates/nav.component.php
+++ components/templates/nav.component.php
@@ -3,7 +3,51 @@ function navHeader(): void
 {
     $user = $_SESSION['user'] ?? null;
     ?>
+    <header>
+        <nav>
+            <ul>
+                <div style="display: flex; align-items: center;">
+                    <li><a href="/index.php">Home</a></li>
+                    <li><a href="/pages/gallery/index.php">Gallery</a></li>
+                    <li><a href="/pages/profile.php">Profile</a></li>
+                    <li><a href="/pages/settings.php">Settings</a></li>
+                    <li>
+                        <form class="logout-form" action="/handlers/auth.handler.php?action=logout" method="POST">
+                            <button type="submit">Logout</button>
+                        </form
+                    </li>
+                </div>
+
+                <?php if ($user): ?>
+                    <li class="user-info">
+                        Logged in as: <?= htmlspecialchars($user['first_name'] . ' ' . $user['last_name']) ?>
+                        (<?= htmlspecialchars($user['role']) ?>)
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </nav>
+    </header>
+
+
     <style>
+        form.logout-form {
+    margin: 0;
+}
+
+        form.logout-form button {
+            background: none;
+            border: none;
+            color: white;
+            font: inherit;
+            cursor: pointer;
+            padding: 0;
+            text-decoration: underline;
+        }
+
+        form.logout-form button:hover {
+            text-decoration: none;
+        }
+
         header {
             background-color: #343a40;
             color: white;
@@ -37,27 +81,6 @@ function navHeader(): void
             font-size: 0.9em;
         }
     </style>
-
-    <header>
-        <nav>
-            <ul>
-                <div style="display: flex; align-items: center;">
-                    <li><a href="/index.php">Home</a></li>
-                    <li><a href="/pages/gallery/index.php">Gallery</a></li>
-                    <li><a href="/pages/profile.php">Profile</a></li>
-                    <li><a href="/pages/settings.php">Settings</a></li>
-                    <li><a href="/handlers/logout.handler.php">Logout</a></li>
-                </div>
-
-                <?php if ($user): ?>
-                    <li class="user-info">
-                        Logged in as: <?= htmlspecialchars($user['first_name'] . ' ' . $user['last_name']) ?>
-                        (<?= htmlspecialchars($user['role']) ?>)
-                    </li>
-                <?php endif; ?>
-            </ul>
-        </nav>
-    </header>
     <?php
 }
 ?>
